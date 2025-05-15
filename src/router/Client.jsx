3891d26a8862a7c939c66691730fd08971ceb6f9
Althafpro192 <124490468+Althafpro192@users.noreply.github.com>
2025-05-16 05:57:34 +0700

    Update Client.jsx

--- src/router/Client.jsx
+++ src/router/Client.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: 'http://192.168.20.179:8200/api/',
 });
 
 client.interceptors.request.use(
